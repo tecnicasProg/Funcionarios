bbf083002022224035f85e54054a04b6e050fa1f
meduardacn <m.eduardacn@gmail.com>
2018-08-28 10:19:08 -0300

    Add files via upload

---
+++ Funcionarios/src/main/java/tecnicas/Funcionarios/App.java
@@ -0,0 +1,13 @@
+package tecnicas.Funcionarios;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Funcionarios/src/test/java/tecnicas/Funcionarios/AppTest.java
@@ -0,0 +1,34 @@
+package tecnicas.Funcionarios;
+
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static TestSuite suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
